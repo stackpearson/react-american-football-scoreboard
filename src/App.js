e18ac23bc72883aea8995e490ab0f0820b4b9dfe
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-05-17 19:54:40 -0600

    added functionality to render user entered team names & reset the score

--- src/App.js
+++ src/App.js
@@ -9,20 +9,23 @@ function App() {
 
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+  const [homeTeam, setHomeTeamName] = useState('Home');
+  const [awayTeam, setAwayTeamName] = useState('Away');
+  let newHomeTeamName = document.querySelector('.homeTeamInput');
 
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">{homeTeam}</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
             <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">{awayTeam}</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
@@ -38,11 +41,32 @@ function App() {
           <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore + 3)}>Home Field Goal</button>
         </div>
+        <div className="reset">
+          <button className="resetButton" onClick = {() => (setAwayScore(0), setHomeScore(0))}>Reset Score</button>
+        </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown" onClick={() => setAwayScore(awayScore + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={() => setAwayScore(awayScore + 3)}>Away Field Goal</button>
         </div>
+        
       </section>
+      
+      <div className="dataInput">
+        
+          <div>
+            <input className="homeTeamInput" placeholder="Home Team Name"></input>
+            <div><button className="homeNameButton" onClick={() => setHomeTeamName(document.querySelector('.homeTeamInput').value)}>Enter</button></div>
+
+          </div>
+          
+          <div>
+            <input className="awayTeamInput" placeholder="Away Team Name"></input>
+            <div><button className="awayNameButton" onClick={() => setAwayTeamName(document.querySelector('.awayTeamInput').value)}>Enter</button></div>
+          </div>
+          
+
+      </div>
+
     </div>
   );
 }
